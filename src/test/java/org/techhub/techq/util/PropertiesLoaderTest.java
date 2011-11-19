cfd08955d2a093111c1da7b1f0b49b963016ed67
Go Tanaka <tanago3@gmail.com>
2011-11-19 20:30:01 +0900

    fixed test error.

--- src/test/java/org/techhub/techq/util/PropertiesLoaderTest.java
+++ src/test/java/org/techhub/techq/util/PropertiesLoaderTest.java
@@ -1,7 +1,10 @@
 package org.techhub.techq.util;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
 import org.junit.Test;
 
 
@@ -9,7 +12,7 @@ public class PropertiesLoaderTest {
 
 	@Test
 	public void testGetKey() throws Exception {
-		PropertiesLoader loader = new PropertiesLoader("webapp_test");
+		PropertiesLoader loader = new PropertiesLoader("target/test-classes/webapp_test.properties");
 		assertThat(loader.get("document.root"), is("/var/www/techq"));
 		assertThat(loader.get("default.document"), is("index.html"));
 	}
